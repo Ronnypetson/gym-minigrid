62fbad6628794cc8ba10471d33b87ee853bd1f9a
Ronnypetson Souza da Silva <rsronnypetson4@gmail.com>
2021-10-31 14:05:59 -0300

    add parsing.py

---
+++ project_RL/parsing.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def linear_parse_observation_to_state(observation):
+    ''' Parse to concatenation of one-hot encodings of observation.
+        Each cell has the (OBJECT_IDX, COLOR_IDX, STATE) format.
+        OBJECT_IDX: 11 possible values
+        COLOR_IDX: 6 possible values
+        STATE: 3 possible values
+        direction: 4 possible values
+    '''
+    image = observation["image"]
+    rows, cols, _ = image.shape
+    feat_possible_values = {0: 11, 1: 6, 2: 3}
+    num_directions = 4
+    binary_features = []
+    for row in range(rows):
+        for column in range(cols):
+            for feat, possible_vals in feat_possible_values.items():
+                one_hot = [0 for _ in range(possible_vals)]
+                one_hot[image[row][column][feat]] = 1
+                binary_features += one_hot
+    direction = [0 for _ in range(num_directions)]
+    direction[observation["direction"]] = 1
+    binary_features += direction
+    binary_features = np.array(binary_features)
+    return binary_features
+
+
+def parse_observation_to_state(observation):
+    return tuple([tuple(observation["image"].flatten()),
+                  observation["direction"]])
