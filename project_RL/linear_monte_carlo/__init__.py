ab19169e460cdfbe4ab2f5d97f430ea881f7879d
Ronnypetson Souza da Silva <rsronnypetson4@gmail.com>
2021-11-02 20:52:18 -0300

    added __init__.py in order to use colab

---
+++ project_RL/linear_monte_carlo/__init__.py
@@ -0,0 +1,2 @@
+import project_RL.linear_monte_carlo.train
+from project_RL.linear_monte_carlo.monte_carlo_agent import *
---
+++ project_RL/monte_carlo/__init__.py
@@ -0,0 +1,2 @@
+import project_RL.monte_carlo.train
+from project_RL.monte_carlo.monte_carlo_agent import *
