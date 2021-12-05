d46416aea339be991f8e0e18b4a126a9cf49ad84
Ronnypetson <rsronnypetson4@gmail.com>
2021-12-05 14:32:36 -0300

    added schedule for epsilon

--- project_RL/batch.py
+++ project_RL/batch.py
@@ -7,7 +7,7 @@ import numpy as np
 def np2tensor(
     x: np.ndarray,
     make_batch: bool=False,
-    ):
+    ) -> torch.Tensor:
     ''' Converts numpy array to tensor, adding a batch
         dimension if "make_batch" is "True".
     '''
@@ -21,7 +21,7 @@ def resize_range(
     x: np.ndarray,
     min_val: np.uint8=0,
     max_val: np.uint8=255
-    ):
+    ) -> np.ndarray:
     ''' Converts values from min_val - max_val to 0 - 1 interval.
     '''
     assert max_val > min_val
@@ -30,7 +30,8 @@ def resize_range(
 
 
 def experience2batches(
-    exp: List[Tuple]
+    exp: List[Tuple],
+    device: str='cpu'
     ):
     ''' Unpacks a experiences into batch tensors.
     '''
@@ -38,17 +39,20 @@ def experience2batches(
     action = []
     reward = []
     new_state = []
-    for s, a, r, s_ in exp:
+    done = []
+    for s, a, r, s_, d in exp:
         state.append(s)
         action.append(a)
         reward.append(r)
         new_state.append(s_)
-    state = torch.cat(state, dim=0)
-    new_state = torch.cat(new_state, dim=0)
-    action = torch.tensor(action)
-    reward = torch.cat(reward, dim=0)
-    assert len(state) == len(new_state) == len(reward) == len(action)
-    return state, action, reward, new_state
+        done.append(d)
+    state = torch.cat(state, dim=0).to(device)
+    new_state = torch.cat(new_state, dim=0).to(device)
+    action = torch.tensor(action).to(device)
+    reward = torch.cat(reward, dim=0).to(device)
+    done = torch.cat(done, dim=0).to(device)
+    assert len(state) == len(new_state) == len(reward) == len(action) == len(done)
+    return state, action, reward, new_state, done
 
 
 class NumpyArrayTransform:
